ee1bc88e0bc9db27c342e0f05fdf45c616aa721e
jacknoble <jack.noble@gmail.com>
2014-01-31 14:48:49 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BanjoChallenge::Application.config.secret_token = '10a616a2de6ac30ba030f4841ab1d9fbafaec009cd11903a585c4af670e04c69819ed14d673d677d5cdcd553e79397abab9e18d5742dd52fb7645fff1b83553e'
