b276ec6a91cbe97574c76e96efe4f6a65d80514e
jacknoble <jack.noble@gmail.com>
2014-02-03 22:34:57 -0800

    add some error catching for api request

--- app/controllers/photo_sets_controller.rb
+++ app/controllers/photo_sets_controller.rb
@@ -2,8 +2,15 @@ class PhotoSetsController < ApplicationController
 	def create
 		@photo_set = PhotoSet.new(params[:set])
 		if @photo_set.save
-			Photo.pull_for_set(@photo_set)
-			redirect_to photo_set_photos_url(@photo_set)
+			begin
+  			Photo.pull_for_set(@photo_set)
+			rescue => e
+				errors =  JSON.parse(e.response)
+  			flash[:errors] = errors['meta']['error_message'].capitalize
+  			redirect_to :back
+  		else
+  			redirect_to photo_set_photos_url(@photo_set)
+			end
 		else
 			flash[:errors] = "Enter latitude then longitude separated by a comma."
 			redirect_to :back
